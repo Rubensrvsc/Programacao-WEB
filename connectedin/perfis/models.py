cee5961af4d6b91bd286786adc3961c725413b9d
Rubensrvsc <Rubensspfc100@gmail.com>
2018-11-20 10:19:51 -0300

    ADD: connectedin

---
+++ connectedin/perfis/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Perfil(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)
+    telefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=255, null=False)
+    contatos  =  models.ManyToManyField('self')
+
+    def convidar(self,perfil_convidado):
+        Convite(solicitante=self,  convidado=perfil_convidado).save()
+
+class Convite(models.Model):
+    solicitante = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='solicitante')
+    convidado = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='convidado')
+
+    def  aceitar(self): 
+        self.convidado.contatos.add(self.solicitante) 
+        self.solicitante.contatos.add(self.convidado) 
+        self.delete()
---
+++ connectedin/perfis/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from .models import *
+# Create your views here.
+def index(request):
+    return render(request,'index.html',{'perfis':Perfil.objects.all()})
+
+def	exibir_perfil(request,	perfil_id):
+    perfil	=	Perfil.objects.get(id=perfil_id) 
+    perfil_logado	=	get_perfil_logado(request) 
+    ja_eh_contato = perfil	in	perfil_logado.contatos.all()
+
+    return	render(request,'perfil.html',{'perfil':perfil,'perfil_logado':get_perfil_logado(request),
+        'ja_eh_contato'	:	ja_eh_contato})
+
+
+def convidar(request,perfil_id):
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
+
+def aceitar(request, convite_id):
+    convite  =  Convite.objects.get(id=convite_id) 
+    convite.aceitar()
+    return  redirect('index')
+
