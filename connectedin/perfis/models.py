3d116d11969a2430fca7456e9c3f986e290bd98c
Rubensrvsc <Rubensspfc100@gmail.com>
2019-01-01 09:40:41 -0300

    ADD: recusar e desfazer amizade

--- connectedin/perfis/models.py
+++ connectedin/perfis/models.py
@@ -1,21 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
 class Perfil(models.Model):
     nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
-    telefone = models.CharField(max_length=15, null=False)
+    telefone = models.CharField(max_length=20, null= False)
     nome_empresa = models.CharField(max_length=255, null=False)
-    contatos  =  models.ManyToManyField('self')
+    contatos = models.ManyToManyField('Perfil')
+    usuario = models.OneToOneField(User, related_name="perfil", on_delete = models.CASCADE,default="", editable=False) 
 
-    def convidar(self,perfil_convidado):
-        Convite(solicitante=self,  convidado=perfil_convidado).save()
+    def __str__(self):
+        return self.nome
+
+    @property 
+    def email(self): 
+        return self.usuario.email
+
+    def convidar(self, perfil_convidado):
+        #if self.pode_convidar(perfil_convidado):
+        convite = Convite(solicitante=self,convidado = perfil_convidado)
+        convite.save()
+
+    def desfazer(self,perfil):
+        self.contatos.remove(perfil)
+        perfil.contatos.remove(self)
+
+    def remove(self,perfil):
+        pass
+
 
 class Convite(models.Model):
-    solicitante = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='solicitante')
-    convidado = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='convidado')
+    solicitante = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='convites_feitos' )
+    convidado = models.ForeignKey(Perfil, on_delete= models.CASCADE, related_name='convites_recebidos')
 
-    def  aceitar(self): 
-        self.convidado.contatos.add(self.solicitante) 
-        self.solicitante.contatos.add(self.convidado) 
+    def aceitar(self):        
+        self.solicitante.contatos.add(self.convidado)
+        self.convidado.contatos.add(self.solicitante)
+        self.delete()
+    
+    def recusar(self):
         self.delete()
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,29 +1,57 @@
+from perfis.models import Perfil, Convite
 from django.shortcuts import render,redirect
-from .models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from django.http import HttpResponseNotFound
+
+@login_required
 def index(request):
-    return render(request,'index.html',{'perfis':Perfil.objects.all()})
+	return render(request, 'index.html',{'perfis' : Perfil.objects.all(),'perfil_logado' : get_perfil_logado(request)})
 
-def	exibir_perfil(request,	perfil_id):
-    perfil	=	Perfil.objects.get(id=perfil_id) 
-    perfil_logado	=	get_perfil_logado(request) 
-    ja_eh_contato = perfil	in	perfil_logado.contatos.all()
+@login_required
+def exibir_perfil(request, perfil_id):
 
-    return	render(request,'perfil.html',{'perfil':perfil,'perfil_logado':get_perfil_logado(request),
-        'ja_eh_contato'	:	ja_eh_contato})
+	perfil = Perfil.objects.get(id=perfil_id)
 
+	return render(request, 'perfil.html',
+		          {'perfil' : perfil, 
+				   'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def convidar(request,perfil_id):
-    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    perfil_logado.convidar(perfil_a_convidar)
+
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	
+	#if(perfil_logado.pode_convidar(perfil_a_convidar)):
+	perfil_logado.convidar(perfil_a_convidar)
+	
+	return  redirect('index')
+
+@login_required
+def get_perfil_logado(request):
+	#if request.user.is_authenticated: 
+	profile=request.user.perfil
+	return profile
+	#else: 
+		#return HttpResponseNotFound('sem perfil')
+
+@login_required
+def recusar(request,convite_id):
+    convite=Convite.objects.get(id=convite_id)
+    convite.recusar()
     return redirect('index')
 
-def get_perfil_logado(request):
-    return Perfil.objects.get(id=1)
-
+@login_required
 def aceitar(request, convite_id):
-    convite  =  Convite.objects.get(id=convite_id) 
-    convite.aceitar()
-    return  redirect('index')
+	convite = Convite.objects.get(id = convite_id)
+	convite.aceitar()
+	return redirect('index')
+
+@login_required
+def desfazer_amizade(request,perfil_id):
+	perfil = Perfil.objects.get(id=perfil_id)
+	perfil.desfazer(get_perfil_logado(request))
+	return redirect('index')
+
 
